ecaf66381aad226d0903fd98cbf8d25df12c1c79
kei55321 <keibal514@gmail.com>
2025-12-31 03:22:27 +0900

    Add files via upload

---
+++ image/a.bat
@@ -0,0 +1,62 @@
+@echo off
+REM ===== Twisted Wonderland Character Folders =====
+
+REM Heartslabyul
+mkdir "Heartslabyul"
+cd "Heartslabyul"
+mkdir "Riddle"
+mkdir "Ace"
+mkdir "Deuce"
+mkdir "Trey"
+mkdir "Cater"
+cd ..
+
+REM Savanaclaw
+mkdir "Savanaclaw"
+cd "Savanaclaw"
+mkdir "Leona"
+mkdir "Ruggie"
+mkdir "Jack"
+cd ..
+
+REM Octavinelle
+mkdir "Octavinelle"
+cd "Octavinelle"
+mkdir "Azul"
+mkdir "Jade"
+mkdir "Floyd"
+cd ..
+
+REM Scarabia
+mkdir "Scarabia"
+cd "Scarabia"
+mkdir "Kalim"
+mkdir "Jamil"
+cd ..
+
+REM Pomefiore
+mkdir "Pomefiore"
+cd "Pomefiore"
+mkdir "Vil"
+mkdir "Epel"
+mkdir "Rook"
+cd ..
+
+REM Ignihyde
+mkdir "Ignihyde"
+cd "Ignihyde"
+mkdir "Idia"
+mkdir "Ortho"
+cd ..
+
+REM Diasomnia
+mkdir "Diasomnia"
+cd "Diasomnia"
+mkdir "Maleus"
+mkdir "Lilia"
+mkdir "Silver"
+mkdir "Sebek"
+cd ..
+
+echo Folders created successfully!
+pause
